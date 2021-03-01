8d03e68e92a08340a03be19ce023a92aa53954df
Franky777shg <frengky.sihombing.777@gmail.com>
2021-03-01 16:06:30 +0700

    review redux thunk

--- App.js
+++ App.js
@@ -3,11 +3,25 @@ import { NavigationContainer } from '@react-navigation/native'
 
 import SplashNav from './src/navigations/SplashNavigation'
 
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import ReduxThunk from 'redux-thunk'
+import allReducer from './src/reducers'
+
+const globalStore = createStore(
+  allReducer,
+  {},
+  composeWithDevTools(applyMiddleware(ReduxThunk))
+)
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <SplashNav/>
-    </NavigationContainer>
+    <Provider store={globalStore}>
+      <NavigationContainer>
+        <SplashNav />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
